7a90147ef15c92667d1c4fe853a001033a092ebb
Dani Nere <danisilvanere@gmail.com>
2024-12-18 16:30:52 -0300

    feat: set up Firebase Realtime Database in the project
    - Added Firebase Realtime Database dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-database")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
