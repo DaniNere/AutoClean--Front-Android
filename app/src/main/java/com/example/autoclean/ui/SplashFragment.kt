71e269ebf3e76005692cf64488c731bab88aaef0
Dani Nere <danisilvanere@gmail.com>
2025-01-04 21:46:06 -0300

    fix: corrigir bugs nas cores e temas, e implementar navegação inicial
    - Ajuste nas cores da status bar e temas para melhor compatibilidade.
    - Correção de erros no seletor de cores.
    - Implementação da navegação inicial no app (SplashFragment para LoginFragment).

--- app/src/main/java/com/example/autoclean/ui/SplashFragment.kt
+++ app/src/main/java/com/example/autoclean/ui/SplashFragment.kt
@@ -1,12 +1,16 @@
 package com.example.autoclean.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.autoclean.R
 import com.example.autoclean.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
 
 
 class SplashFragment : Fragment() {
@@ -20,6 +24,17 @@ class SplashFragment : Fragment() {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth,3000 )
+
+    }
+    private fun checkAuth(){
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
