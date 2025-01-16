01b587d855d80ebd07c9bbeb0ced2e25bd089383
Dani Nere <danisilvanere@gmail.com>
2025-01-16 19:04:42 -0300

    :sparkles: feat: create layout for kit validation with responsive design
    - Created the initial layout for the kit validation screen.
    - Added a LinearLayout for video or image upload with a dotted border and centered icon.
    - Implemented fixed dimensions to ensure responsiveness across different screen sizes.
    - Included contentDescription attribute for accessibility in the ImageView.
    - Validated layout compatibility and responsiveness for various resolutions.

---
+++ app/src/main/java/com/example/autoclean/ui/auth/ValidationKitFragment.kt
@@ -0,0 +1,30 @@
+package com.example.autoclean.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.autoclean.R
+import com.example.autoclean.databinding.FragmentValidationKitBinding
+
+
+class ValidationKitFragment : Fragment() {
+    private var _binding: FragmentValidationKitBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_validation_kit, container, false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
