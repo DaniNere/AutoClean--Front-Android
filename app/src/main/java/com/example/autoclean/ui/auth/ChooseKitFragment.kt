948d632c9b60e24e39c0f4cccec4fa510edc2fe0
Dani Nere <danisilvanere@gmail.com>
2025-01-17 18:10:15 -0300

    :sparkles:feat: Add conditional navigation based on selected card
    - Implements card selection logic for navigation between fragments
    - Adds listener on Continue button to direct the user to the corresponding kit fragment
    - Updates navigation flow to include transitions for kit 1 and kit 2 fragments

--- app/src/main/java/com/example/autoclean/ui/auth/ChooseKitFragment.kt
+++ app/src/main/java/com/example/autoclean/ui/auth/ChooseKitFragment.kt
@@ -1,6 +1,7 @@
 package com.example.autoclean.ui.auth
 
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,12 +10,11 @@ import androidx.navigation.fragment.findNavController
 import com.example.autoclean.R
 import com.example.autoclean.databinding.FragmentChooseKitBinding
 
-
-
 class ChooseKitFragment : Fragment() {
 
     private var _binding: FragmentChooseKitBinding? = null
     private val binding get() = _binding!!
+    private var selectedCard: CardView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,22 +26,35 @@ class ChooseKitFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initListeners()
-
     }
 
-    private fun initListeners(){
+    private fun initListeners() {
 
-        binding.btnContinue.setOnClickListener{
-            findNavController().navigate(R.id.action_cleaningkitmanagerFragment_to_validationKitFragment)
+        val card1 = binding.root.findViewById<CardView>(R.id.card1)
+        val card2 = binding.root.findViewById<CardView>(R.id.card2)
+
+        val cards = listOf(card1, card2)
+
+        cards.forEach { card ->
+            card.setOnClickListener {
+                selectedCard?.setBackgroundResource(R.drawable.border_unselected)
+                card.setBackgroundResource(R.drawable.border_selected)
+                selectedCard = card
+            }
         }
 
+        binding.btnContinue.setOnClickListener{
+            if (selectedCard == card1){
+                findNavController().navigate(R.id.action_cleaningkitmanagerFragment_to_validationKitFragment)
+            } else if (selectedCard ==card2){
+                findNavController().navigate(R.id.action_cleaningkitmanagerFragment_to_cleaningKitPaymentFragment)
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
